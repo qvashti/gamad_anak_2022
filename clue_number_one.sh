959c4f70189948915c8cf78f511261b68412a09b
Queen Vashti <qvashti321@gmail.com>
2022-03-13 15:41:10 +0200

    Added Clue #1

---
+++ clue_number_one.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Hello......"
+sleep 2
+echo "....Roni"
+sleep 2
+echo "Are you ready"
+sleep 1
+echo "For the next clue?"
+echo ""
+sleep 3
+echo "What does the Queen drink..."
+sleep 1
+echo "...Instead of coffee?"
