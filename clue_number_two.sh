4b0894717daac69d33158f0aae15b461fed28bfa
Queen Vashti <qvashti321@gmail.com>
2022-03-13 16:03:49 +0200

    Added Clue #2

---
+++ clue_number_two.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Hello Again......"
+sleep 2
+echo "....Roni"
+sleep 2
+echo "You're getting good at this"
+echo ""
+sleep 3
+echo "If you want a snack..."
+sleep 1
+echo "...check the top drawer under the printer"
